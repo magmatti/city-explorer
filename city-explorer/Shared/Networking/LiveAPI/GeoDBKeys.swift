f49f7b46178233ee11c3ee740f0b4b2d32a8bfaa
Mateusz Wójtowicz <matiwojt@gmail.com>
2025-11-16 22:00:26 +0100

    remove API keys

--- city-explorer/Shared/Networking/LiveAPI/GeoDBKeys.swift
+++ city-explorer/Shared/Networking/LiveAPI/GeoDBKeys.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 enum GeoDBKeys {
-    static let apiKey = "4f49dfd059msh1562a0d8e6bf332p1b1d0djsn49956b87a9c1"
-    static let host = "wft-geo-db.p.rapidapi.com"
+    static let apiKey = ""
+    static let host = ""
 }
